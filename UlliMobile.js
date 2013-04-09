e94781d7cf9db5e8c3375c15e4e3c8aa76cc1fd1
juju <juliette@shic.fr>
2013-04-09 13:14:02 +0200

    menu dynamic

--- UlliMobile.js
+++ UlliMobile.js
@@ -1,110 +1,196 @@
 var UlliMobile = {
 
-    container : $("<div></div>"),
+    container:$("<div class='page'></div>"),
 
-    generateContainer:function(){
-
+    generateContainer:function () {
         UlliMobile.container.css("position", "absolute");
-        UlliMobile.container.css("border", "1px solid red");
-        UlliMobile.container.css("width", "500px");
-        UlliMobile.container.css("height", "500px");
-        UlliMobile.container.css("right", "10px");
     },
-
-
-    searchUl:function (el) {
-        //
-        var listul = $(el).find("ul");
-
-        for (var i = 0; i < listul.length; i++) {
-
-            // boucle for search an ul inside the ul
-            UlliMobile.searchUl(listul[i].firstChild);
-
-            // ul
-            var original = $(listul[i]);
-
-            // create ul without the nodes inside
-            var ecran = original.clone();
-            ecran.find("li ul").remove();
-
-            // create title
-            var elparent = $(original.parent().children().first());
-            var title = elparent.clone();
-            ecran.prepend(title);
-
-            UlliMobile.container.append(ecran);
-            ecran.css("border", "1px solid red");
-
-        }
-
-    },
-
-
 
     generateScreens:function () {
+        $(".ulli-mobile").append(UlliMobile.container);
+    },
 
-        $("body").append(UlliMobile.container);
 
-        /*var ecran = $(".ulli-mobile").clone();
+    ///
+    DisplayUl:function (el, li, way) {
 
-        container.append(ecran);
+        var firstli = $(el).find(li);
 
-        var toto = $('#toto');
-        toto.data("ecran", ecran);
 
-        toto.on('click', function (e) {
-            if ($(this).data("ecran").css("display") == "none") {
-                $(this).data("ecran").css("display", "block");
-            } else {
-                $(this).data("ecran").css("display", "none")
+        for (var i = 0; i < firstli.length; i++) {
+
+            // head li
+            var headli = $(firstli[i]).children().first();
+            // first ul in head li
+            var firstul = $(UlliMobile.setFirstUl(headli));
+            headli.data('firstul', firstul);
+
+
+            // create icons for each headli (just one time only)
+            if (firstul.length != 0) {
+                if (headli.find('.more').length == 0) {
+                    var more = $("<div class='more'>+</div>");
+                    headli.append(more);
+                    var less = $("<div class='less'>-</div>");
+                    headli.append(less);
+                    var arrow = $("<div class='arrow'>></div>");
+                    headli.append(arrow);
+
+                    // hide icons according to the way to open the first ul
+                    if (way == "accordion") {
+                        arrow.addClass("erase");
+                        more.addClass("erase");
+                    }
+                    if (way == "page") {
+                        more.addClass("erase");
+                        less.addClass("erase");
+                    }
+                }
+                headli.css("cursor","pointer");
             }
-        });*/
+
+
+            headli.on("click", function (e) {
+
+                var el = $(this);
+                var firstul = el.data('firstul');
+
+                // if there is ul
+                if (firstul.length != 0) {
+
+                    if (way == "accordion") {
+                        // open/close ul
+                        if (!el.hasClass("in")) {
+                            el.addClass("in");
+                            firstul.addClass("active");
+
+                            // hide/show icons
+                            el.find('.more').removeClass("erase");
+                            el.find('.less').addClass("erase");
+
+                        } else {
+                            el.removeClass("in");
+                            firstul.removeClass("active");
+
+                            // hide/show icons
+                            el.find('.more').addClass("erase");
+                            el.find('.less').removeClass("erase");
+
+                        }
+                    } else if (way == "page") {
+
+                        UlliMobile.ClonePage(el, firstul);
+
+                    }
+
+                    //don't go to the link, a href inactive
+                    return false;
+                }
+            })
+        }
+    },
+
+    ///
+    setFirstUl:function (el) {
+        // search ul inside il
+        var firstul = el.parent().find("ul").first();
+        return firstul;
+    },
+
+
+    ClonePage:function (el, ul) {
+        // create ul without the nodes inside
+        var ecran = ul.clone();
+        //ecran.find("li ul").remove();
+
+        // create title
+        var elparent = $(el.parent().children().first());
+        var title = elparent.clone();
+        title.find('.arrow').addClass("erase");
+        ecran.prepend(title);
+
+        // if there isn't a on title then cursor:inherit
+        if(title.find('a').length == 0){
+            title.css("cursor","inherit");
+        }
+
+        // find ul parent
+        var parentul = $(el.closest("ul"));
+
+        // hide parentful
+        parentul.addClass("erase");
+
+        UlliMobile.CheckMultiple(parentul);
+
+        // create back
+        var back = $("<div style='border-left: 10px solid #000;'>&nbsp;</div>");
+        back.data("parentul", parentul);
+        back.data("ecran", ecran);
+
+        back.on('click', function (e) {
+
+            var parentul = $(this).data("parentul");
+            parentul.removeClass("erase");
+
+            var ecran = $(this).data("ecran");
+            ecran.addClass("erase");
+
+            UlliMobile.CheckMultiple(parentul);
+        })
+        ecran.prepend(back);
+
+        UlliMobile.container.append(ecran);
+
+        // change the setup for display the next ul
+        if ($("body").find(".ulli-accordion-page").length == 1) {
+            UlliMobile.DisplayUl(ecran, "li", "page");
+        } else if ($("body").find(".ulli-page-accordion").length == 1) {
+            UlliMobile.DisplayUl(ecran, "li", "accordion");
+        } else {
+            UlliMobile.DisplayUl(ecran, "li", "page");
+        }
+    },
+
+    CheckMultiple:function (parentul) {
+        // if we are on ulli-mobile multiple, we have to hide the first ul
+        if (parentul.hasClass("active") == true) {
+            var firstul = $(".ulli-mobile > ul");
+            if (!firstul.hasClass("erase")) {
+                firstul.addClass("erase");
+            } else {
+                firstul.removeClass("erase");
+            }
+        }
     }
+
+
+
 }
 
 
-// function for the first li
-$("body").on("click", ".ulli-mobile > li", function (e) {
-
-    var el = $(this);
-
-    // search li inside ul
-    var inli = el.find("ul").first();
-
-    
-
-
-    // if there is li
-    if (inli.length != 0) {
-
-        // open/close ul
-        if (!el.hasClass("in")) {
-            el.addClass("in");
-            inli.addClass("active");
-        } else {
-            el.removeClass("in");
-            inli.removeClass("active");
-        }
-
-        //don't go to the link, a href inactive
-        return false;
-    }
-
-})
-
-$("body").on("click", "li ul li", function (e) {
-
-    var el = $(this);
-
-    // search li inside ul
-    var inli = el.find("ul").first();
-
-
-});
 UlliMobile.generateContainer();
 UlliMobile.generateScreens();
-UlliMobile.searchUl(".ulli-mobile");
+//
+
+
+if ($("body").find(".ulli-accordion").length == 1) {
+    UlliMobile.DisplayUl(".ulli-mobile", "li", "accordion");
+
+} else if ($("body").find(".ulli-page").length == 1) {
+    UlliMobile.DisplayUl(".ulli-mobile", "li", "page");
+
+} else if ($("body").find(".ulli-accordion-page").length == 1) {
+    UlliMobile.DisplayUl(".ulli-mobile", "> ul > li", "accordion");
+    UlliMobile.DisplayUl(".ulli-mobile", "li ul li", "page");
+} else if ($("body").find(".ulli-page-accordion").length == 1) {
+    UlliMobile.DisplayUl(".ulli-mobile", "> ul > li", "page");
+    UlliMobile.DisplayUl(".ulli-mobile", "li ul li", "accordion");
+} else {
+    UlliMobile.DisplayUl(".ulli-mobile", "li", "accordion");
+}
+
+
+
 
 
 
